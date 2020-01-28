cbe22df4ebafcba5054550d27840b06572a5b740
Marco Maggi <marco.maggi-ipsu@poste.it>
2020-01-28 10:36:13 +0100

    split header files into two

--- src/mmux-emacs-gmp-internals.h
+++ src/mmux-emacs-gmp-internals.h
@@ -30,12 +30,8 @@
  ** Headers.
  ** ----------------------------------------------------------------- */
 
-#ifdef HAVE_CONFIG_H
-#  include <config.h>
-#endif
+#include "mmux-emacs-internals.h"
 #include "mmux-emacs-gmp.h"
-#include <assert.h>
-#include <stdio.h>
 
 
 /** --------------------------------------------------------------------
@@ -55,29 +51,8 @@
 
 
 /** --------------------------------------------------------------------
- ** Type definitions.
+ ** MMUX Emacs GMP type definitions.
  ** ----------------------------------------------------------------- */
-
-typedef emacs_value function_implementation_t (emacs_env *env, ptrdiff_t nargs, emacs_value args[], void *data);
-
-typedef struct module_function_t	module_function_t;
-
-struct module_function_t {
-  char const			* name;
-  function_implementation_t	* implementation;
-  ptrdiff_t			min_arity;
-  ptrdiff_t			max_arity;
-  char const			* documentation;
-};
-
-/* ------------------------------------------------------------------ */
-
-typedef signed   int		mmux_sint_t;
-typedef unsigned int		mmux_uint_t;
-typedef signed   long int	mmux_slint_t;
-typedef unsigned long int	mmux_ulint_t;
-
-/* ------------------------------------------------------------------ */
 
 typedef __gmp_randstate_struct *	mmux_gmp_randstate_t;
 
@@ -86,117 +61,6 @@ typedef __gmp_randstate_struct *	mmux_gmp_randstate_t;
  ** Constants.
  ** ----------------------------------------------------------------- */
 
-/* This is required by GNU Emacs' API. */
-mmux_emacs_gmp_decl int  plugin_is_GPL_compatible;
-
-
-/** --------------------------------------------------------------------
- ** MMUX Emacs generic inline functions.
- ** ----------------------------------------------------------------- */
-
-static inline void *
-mmux_emacs_get_ptr (emacs_env * env, emacs_value arg)
-{
-  return env->get_user_ptr(env, arg);
-}
-
-static inline intmax_t
-mmux_emacs_get_int (emacs_env * env, emacs_value arg)
-{
-  return env->extract_integer(env, arg);
-}
-
-static inline double
-mmux_emacs_get_float (emacs_env * env, emacs_value arg)
-{
-  return env->extract_float(env, arg);
-}
-
-static inline mmux_ulint_t
-mmux_emacs_get_ulint (emacs_env * env, emacs_value arg)
-{
-  return ((mmux_ulint_t)(mmux_emacs_get_int(env, arg)));
-}
-
-static inline mmux_slint_t
-mmux_emacs_get_slint (emacs_env * env, emacs_value arg)
-{
-  return ((mmux_slint_t)(mmux_emacs_get_int(env, arg)));
-}
-
-static inline mmux_ulint_t
-mmux_emacs_get_uint (emacs_env * env, emacs_value arg)
-{
-  return ((mmux_uint_t)(mmux_emacs_get_int(env, arg)));
-}
-
-static inline mmux_sint_t
-mmux_emacs_get_sint (emacs_env * env, emacs_value arg)
-{
-  return ((mmux_sint_t)(mmux_emacs_get_int(env, arg)));
-}
-
-/* ------------------------------------------------------------------ */
-
-static inline emacs_value
-mmux_emacs_make_nil (emacs_env * env)
-{
-  return env->intern(env, "nil");
-}
-
-static inline emacs_value
-mmux_emacs_make_true (emacs_env * env)
-{
-  return env->intern(env, "t");
-}
-
-static inline emacs_value
-mmux_emacs_make_boolean (emacs_env * env, int val)
-{
-  return ((val)? mmux_emacs_make_true(env) : mmux_emacs_make_nil(env));
-}
-
-static inline emacs_value
-mmux_emacs_make_int (emacs_env * env, intmax_t arg)
-{
-  return env->make_integer(env, arg);
-}
-
-static inline emacs_value
-mmux_emacs_make_float (emacs_env * env, double arg)
-{
-  return env->make_float(env, arg);
-}
-
-static inline emacs_value
-mmux_emacs_make_uint (emacs_env * env, mmux_uint_t arg)
-{
-  return mmux_emacs_make_int(env, (intmax_t)arg);
-}
-
-static inline emacs_value
-mmux_emacs_make_sint (emacs_env * env, mmux_sint_t arg)
-{
-  return mmux_emacs_make_int(env, (intmax_t)arg);
-}
-
-static inline emacs_value
-mmux_emacs_make_ulint (emacs_env * env, mmux_ulint_t arg)
-{
-  return mmux_emacs_make_int(env, (intmax_t)arg);
-}
-
-static inline emacs_value
-mmux_emacs_make_slint (emacs_env * env, mmux_slint_t arg)
-{
-  return mmux_emacs_make_int(env, (intmax_t)arg);
-}
-
-static inline emacs_value
-mmux_emacs_make_string (emacs_env * env, char const * strptr, size_t strlen)
-{
-  return env->make_string(env, strptr, strlen);
-}
 
 
 /** --------------------------------------------------------------------
